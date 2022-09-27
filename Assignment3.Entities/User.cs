3d413816d52a0f6947144ca52c994fd6a8ad0adb
Emilia-Victoria <emiliav.helsted@gmail.com>
2022-09-27 13:16:20 +0200

    Add fields to User

--- Assignment3.Entities/User.cs
+++ Assignment3.Entities/User.cs
@@ -1,5 +1,14 @@
 namespace Assignment3.Entities;
+using System.ComponentModel.DataAnnotations;
 
 public class User
 {
+    int Id {get; set;}
+    [Required]
+    [StringLength(100)]
+    public String Name {get; set;}
+    [Required]
+    [StringLength(100)]
+    public String Email {get; set;}
+    public virtual IEnumerable<Task> Tasks { get; set; }
 }
