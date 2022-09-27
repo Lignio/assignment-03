340d40f6c990407c486dfdb6a9e1ae0b277f71ce
Oliver Flyckt Wilhjelm <olfw@itu.dk>
2022-09-27 13:18:57 +0200

    Task class updated

--- Assignment3.Entities/Task.cs
+++ Assignment3.Entities/Task.cs
@@ -1,5 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Assignment3.Entities;
 
 public class Task
 {
+    public int Id;
+
+    [Required]
+    [StringLength(50)]
+    public string Title {get; set;} 
+
+    public User AssignedTo {get; set;} 
+
+    [StringLength(100)]
+    public string description {get; set;}
+    public enum State {New, Active, Resolved, Closed, Removed}
+
+    [Required]
+    public State state{get; set;}
+    public IEnumerable<Tag> Tags;
 }
