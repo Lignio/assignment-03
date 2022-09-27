3365cf9e6f78d7f68ff64b4805e88bc46a7ef405
Andreas <andreasmwulff@gmail.com>
2022-09-27 13:40:45 +0200

    Tag entitites

--- Assignment3.Entities/Tag.cs
+++ Assignment3.Entities/Tag.cs
@@ -1,5 +1,15 @@
 namespace Assignment3.Entities;
+using System.ComponentModel.DataAnnotations;
 
 public class Tag
 {
+    [Index(nameof(Tag.Name), IsUnique = true)]
+    int id {get; set;}
+    
+    [StringLength(50)]
+    [Required]
+    public String Name {get; set;}
+
+
+    public virtual ICollection<Task> Tasks {get; set;}
 }
