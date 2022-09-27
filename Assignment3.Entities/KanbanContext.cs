dd1083e5c67173d2dc3efb053c06873a9dec543c
Oliver Flyckt Wilhjelm <olfw@itu.dk>
2022-09-27 13:44:59 +0200

    wip kanbancontext

--- Assignment3.Entities/KanbanContext.cs
+++ Assignment3.Entities/KanbanContext.cs
@@ -1,5 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace Assignment3.Entities;
 
-public class KanbanContext
+public class KanbanContext : DbContext
 {
+    public DbSet<Task> Tasks => Set<Task>();
+    public DbSet<User> Users => Set<User>();
+    public DbSet<Tag> Tags => Set<Tag>();
+
+
+    public KanbanContext(DbContextOptions<KanbanContext> options) : base(options) {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder){
+        modelBuilder.Entity<Task>().HasIndex();
+        modelBuilder.Entity<Task>().Property(e => e.state).HasConversion(
+            v => v.ToString(),
+            v => (State)Enum.Parse(typeof(State), v));
+        modelBuilder.Entity<User>().HasIndex(c => c.Email).IsUnique();  
+        modelBuilder.Entity<Tag>().HasIndex(c=>c.Name).IsUnique();  
+
+
+    }
+
 }
