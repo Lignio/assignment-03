beb09c80677fb5777b04c0060dcc2ccaff64978f
Andreas <andreasmwulff@gmail.com>
2022-09-28 16:05:05 +0200

    TaskRepository so far

--- Assignment3.Entities/TaskRepository.cs
+++ Assignment3.Entities/TaskRepository.cs
@@ -1,5 +1,30 @@
 namespace Assignment3.Entities;
+using Assignment3.Core;
 
-public class TaskRepository
-{
+public sealed class TaskRepository
+{   
+    public State Delete(int taskId)
+    {   State state;
+
+        if(state == State.New) 
+        {
+            state = State.Deleted;
+        }
+        else if(state == State.Active)
+        {
+            state = State.Removed;
+        }
+        else 
+        {
+            return Conflict;
+        }
+    }
+
+    public (State, TaskDTO) Create(TaskCreateDTO task) 
+    {
+        State state;
+        state = State.New;
+    }
+
+    public 
 }
